e2c4d1e17b370bdad5d4bd5558eaed4385e21ce5
Fredrik Strupe <fredrik@strupe.net>
2016-08-11 13:21:36 +0200

    Pause probing script when updating, and use wlan MAC (not eth)

--- roles/common/files/wifi_scripts/generate_probe_id.sh
+++ roles/common/files/wifi_scripts/generate_probe_id.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-probe_id=$(ifconfig eth0 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/://g')
-echo $probe_id > probe_id.txt
+
+mac=$(ifconfig wlan0 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/://g')
+# The REALTEK check is to prevent selection of the interval wifi card on the rpi3
+if [[ "$mac" == "" || $(iwconfig wlan0 | grep REALTEK) == "" ]]; then
+    mac=$(ifconfig wlan1 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/://g')
+    if [[ "$mac" == "" || $(iwconfig wlan1 | grep REALTEK) == "" ]]; then
+        echo "Unable to read wlan MAC"
+        mac=''
+    fi
+fi
+
+echo "$mac" > probe_id.txt
+
+# probe_id=$(ifconfig eth0 | grep -oE '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sed 's/://g')
+# echo $probe_id > probe_id.txt
 
 # The existing scripts merge the ID with the measurement name
 # by themselves, which makes it cumbersome to parse (and also
