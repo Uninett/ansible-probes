b165fe694f77f53a04828b9092cb2757fe8f21ba
Fredrik Strupe <fredrik@strupe.net>
2017-03-22 13:57:51 +0100

    Ignore values from curl timeout & set bwctl interval to 60 min

--- roles/common/files/wifi_scripts/check_http_v4.sh
+++ roles/common/files/wifi_scripts/check_http_v4.sh
@@ -3,7 +3,12 @@
 . ${SCRIPT_DIR}probe.parameters
 . ${SCRIPT_DIR}probe.id
 
-curl -s -w '\nDNS:\t%{time_namelookup}\nConn:\t%{time_connect}\nPreXf:\t%{time_pretransfer}\nStartXf:\t%{time_starttransfer}\nHTTP:\t%{time_total}\n' -o /dev/null http://www.google.com > ${SCRIPT_DIR}http.log
+curl -s -w '\nDNS:\t%{time_namelookup}\nConn:\t%{time_connect}\nPreXf:\t%{time_pretransfer}\nStartXf:\t%{time_starttransfer}\nHTTP:\t%{time_total}\n' -o /dev/null --connect-timeout 20 http://www.google.com > ${SCRIPT_DIR}http.log
+
+# Ignore values if curl timed out
+if [[ "$?" != "0" ]]; then
+    exit 1
+fi
 
 dns=$(cat ${SCRIPT_DIR}http.log | grep DNS | awk '{print $2}')
 http=$(cat ${SCRIPT_DIR}http.log | grep HTTP | awk '{print $2}')
