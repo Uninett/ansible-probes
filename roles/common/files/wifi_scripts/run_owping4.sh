cbb3b9d01c9a03d69d89083ee10bbc36fc3b0222
Fredrik Strupe <fredrik@strupe.net>
2017-03-29 16:11:13 +0200

    Extract more data from owping scripts

--- roles/common/files/wifi_scripts/run_owping4.sh
+++ roles/common/files/wifi_scripts/run_owping4.sh
@@ -17,3 +17,15 @@ echo $I " owj_v4_"$1  $owj_v4 | awk '{print $1 " " $2 $3 " " $4}' >> ${SCRIPT_DI
 
 owpl_v4=$(cat ${SCRIPT_DIR}owamp_out_v4.log  | grep 'lost' | awk '{print $5}' | awk 'BEGIN {FS="("} {print $2}' | awk 'BEGIN {FS=")"} {print $1}' | awk 'BEGIN {FS="%"} {print $1}')
 echo $I " owpl_v4_"$1  $owpl_v4 | awk '{print $1 " " $2 $3 " " $4}' >> ${SCRIPT_DIR}results_report
+
+delay_min=$(awk '/one-way delay/{ print $5 }' ${SCRIPT_DIR}owamp_out_v4.log | awk 'BEGIN {FS="/"} { print $1 }')
+echo "ow_delay_min_v4_$1" "$delay_min" >> ${SCRIPT_DIR}results_report
+
+delay_median=$(awk '/one-way delay/{ print $5 }' ${SCRIPT_DIR}owamp_out_v4.log | awk 'BEGIN {FS="/"} { print $2 }')
+echo "ow_delay_median_v4_$1" "$delay_median" >> ${SCRIPT_DIR}results_report
+
+delay_max=$(awk '/one-way delay/{ print $5 }' ${SCRIPT_DIR}owamp_out_v4.log | awk 'BEGIN {FS="/"} { print $3 }')
+echo "ow_delay_max_v4_$1" "$delay_max" >> ${SCRIPT_DIR}results_report
+
+delay_error=$(awk '/one-way delay/{ print $7 }' ${SCRIPT_DIR}owamp_out_v4.log | grep -oE '[0-9]+\.[0-9]+')
+echo "ow_delay_error_v4_$1" "$delay_error" >> ${SCRIPT_DIR}results_report
--- roles/common/files/wifi_scripts/run_owping6.sh
+++ roles/common/files/wifi_scripts/run_owping6.sh
@@ -17,3 +17,15 @@ echo $I " owj_v6_"$1  $owj_v6 | awk '{print $1 " " $2 $3 " " $4}' >> ${SCRIPT_DI
 
 owpl_v6=$(cat ${SCRIPT_DIR}owamp_out_v6.log  | grep 'lost' | awk '{print $5}' | awk 'BEGIN {FS="("} {print $2}' | awk 'BEGIN {FS=")"} {print $1}' | awk 'BEGIN {FS="%"} {print $1}')
 echo $I " owpl_v6_"$1  $owpl_v6 | awk '{print $1 " " $2 $3 " " $4}' >> ${SCRIPT_DIR}results_report
+
+delay_min=$(awk '/one-way delay/{ print $5 }' ${SCRIPT_DIR}owamp_out_v6.log | awk 'BEGIN {FS="/"} { print $1 }')
+echo "ow_delay_min_v6_$1" "$delay_min" >> ${SCRIPT_DIR}results_report
+
+delay_median=$(awk '/one-way delay/{ print $5 }' ${SCRIPT_DIR}owamp_out_v6.log | awk 'BEGIN {FS="/"} { print $2 }')
+echo "ow_delay_median_v6_$1" "$delay_median" >> ${SCRIPT_DIR}results_report
+
+delay_max=$(awk '/one-way delay/{ print $5 }' ${SCRIPT_DIR}owamp_out_v6.log | awk 'BEGIN {FS="/"} { print $3 }')
+echo "ow_delay_max_v6_$1" "$delay_max" >> ${SCRIPT_DIR}results_report
+
+delay_error=$(awk '/one-way delay/{ print $7 }' ${SCRIPT_DIR}owamp_out_v6.log | grep -oE '[0-9]+\.[0-9]+')
+echo "ow_delay_error_v6_$1" "$delay_error" >> ${SCRIPT_DIR}results_report
