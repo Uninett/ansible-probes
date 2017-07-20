84a3fa14856e2a2f4e9bb7e67c805a7a561867e9
Jakob Notland <jakob_sn_@hotmail.com>
2017-07-20 14:11:54 +0200

    sørge for å kille wpa_supplicant helt og restarte networking, hvis ikke fungerer det ikke på raspbian

--- roles/common/files/wifi_scripts/stop_wireless.sh
+++ roles/common/files/wifi_scripts/stop_wireless.sh
@@ -7,8 +7,12 @@ sudo /sbin/wpa_cli logoff
 # echo "Killing whatever is left..."
 
 sudo /sbin/wpa_cli terminate
+sudo killall wpa_supplicant
 
 # IPv4 address release
 
 sudo dhclient -r wlan0
 
+# Restart networking
+
+sudo /etc/init.d/networking restart
