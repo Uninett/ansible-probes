7bc28f7528deb91cecdedb4561c6127be6d99d04
Jakob Notland <jakob_sn_@hotmail.com>
2017-08-09 15:11:30 +0200

    modify access permissions to allow ansible user to read and send content to host

--- roles/lxc/generate_debootstrap.sh
+++ roles/lxc/generate_debootstrap.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# Unstable debootstrap version(1.0.86~bpo8+1 ->) may be required to be able to run second stage on host
+
+if [[ "${EUID}" != 0 ]]; then
+    echo "[!] Script must be run as root"
+    exit
+fi
+
 echo "[+] Set up stable debian system"
+# Executes debootstrap foreign stage to create system for another architecture
 debootstrap --foreign --arch armhf stable rootfs http://ftp.debian.org/debian/
+
+echo "[+] Alter permissions"
+# Must alter permissions to allow ansible to read the file when copying to host
+chmod 744 rootfs/root/
