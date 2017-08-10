f46746011b7947335d32f9967e851a48aa440cc4
Jakob Notland <jakob_sn_@hotmail.com>
2017-08-10 10:56:48 +0200

    doing most of the things main.yml used to do on update

--- roles/lxc/generate_debootstrap.sh
+++ roles/lxc/generate_debootstrap.sh
@@ -9,8 +9,42 @@ fi
 
 echo "[+] Set up stable debian system"
 # Executes debootstrap foreign stage to create system for another architecture
-debootstrap --foreign --arch armhf stable rootfs http://ftp.debian.org/debian/
+debootstrap --include ntp,fake-hwclock,rsync,bwctl-client,iperf3,curl,bc,python3,python3-pip,dnsutils,ntp,jq \
+--foreign --arch armhf stable rootfs http://ftp.debian.org/debian/
 
 echo "[+] Alter permissions"
-# Must alter permissions to allow ansible to read the file when copying to host
+# Must alter permissions to allow ansible to read the root folder when copying to host
 chmod 744 rootfs/root/
+
+echo "[+] Transfer owping"
+cp ../../common/files/owping rootfs/usr/bin/owping
+
+echo "[+] Alter permissions"
+chmod 0755 rootfs/usr/bin/owping
+
+echo "[+] Touch script directory"
+mkdir rootfs/root/scripts
+
+echo "[+] Transfer scripts"
+cp ../../common/files/wifi_scripts/ rootfs/root/scripts/
+
+echo "[+] Transfer templates"
+cp ../../common/templates rootfs/root/scripts/
+
+echo "[+] Transfer systemd unit files"
+cp ../../common/templates/make_ramdisk.service rootfs/etc/systemd/system/
+cp ../../common/templates/wifi_probing.service rootfs/etc/systemd/system/
+
+echo "[+] Transfer control program"
+cp ../../common/files/control_program.py rootfs/root/scripts/
+cp ../../common/files/control_program_wrapper.sh rootfs/root/scripts/
+
+
+# TODO: Handle script & db configs
+""" After deployment:
+Enable and start services,
+pip3 install elasticsearch,
+wpa_supplicant,
+Use default influxdb config if specified,
+Reload systemd unit files
+"""
